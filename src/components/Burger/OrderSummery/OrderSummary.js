3dce5ec8203c1ad87191143d3b0a90ce23fd625e
rosseniv97 <rosseniv97@gmail.com>
2019-10-10 12:30:03 +0300

    fix filename 'Aux' to Auxilary

--- src/components/Burger/OrderSummery/OrderSummary.js
+++ src/components/Burger/OrderSummery/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import classes from './Modal.css'
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Auxilary'
 const orderSummary = (props)=>{
 
     const ingredientSummary =Object.keys(props.ingredients)
